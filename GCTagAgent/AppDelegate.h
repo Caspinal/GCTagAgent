ee2c8d001c3511a91ab2347d1b90fefe34498598
Connor Aspinall <piefacevian@googlemail.com>
2020-11-28 17:29:59 +0000

    Initial Commit

---
+++ GCTagAgent/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  GCTagAgent
+//
+//  Created by Connor Aspinall on 28/11/2020.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ GCTagAgent/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  GCTagAgent
+//
+//  Created by Connor Aspinall on 28/11/2020.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
